415f729a680f37ec9081c92d38f375e860ded367
babaquero07 <brayanalexanderb@gmail.com>
2023-06-01 16:42:43 -0500

    feat: rights to Javascript Mastery

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
   Navbar,
   Tech,
   Works,
-  StarsCanvas,
+  // StarsCanvas,
 } from "./components";
 
 const App = () => {
@@ -27,9 +27,23 @@ const App = () => {
         {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
+      <footer>
+        <p className="text-center">
+          © Adapted from{" "}
+          <a
+            className="blue-text-gradient"
+            href="https://www.youtube.com/@javascriptmastery"
+            rel="noreferrer"
+            target="_blank"
+          >
+            JavaScript Mastery
+          </a>
+          . All rights reserved.
+        </p>
+      </footer>
     </BrowserRouter>
   );
 };
