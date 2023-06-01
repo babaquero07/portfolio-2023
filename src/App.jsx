29cd95409ce7734cdeaa17beefb44336a85da913
babaquero07 <brayanalexanderb@gmail.com>
2023-06-01 15:41:56 -0500

    feat: Contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   About,
   Contact,
   Experience,
-  Feedbacks,
+  // Feedbacks,
   Hero,
   Navbar,
   Tech,
@@ -24,7 +24,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
